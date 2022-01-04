3870ffe9c47ed3962bfe82bb8f37c8b1f1f1aeb2
James Tanner <james@northroad-craftworks.com>
2022-01-03 22:27:28 -0500

    Fix: Log file formats

--- src/utilities/logger.js
+++ src/utilities/logger.js
@@ -4,6 +4,7 @@ const { createLogger, format, transports } = winston;
 
 export const consoleTransport = new transports.Console({
     format: format.combine(
+        format.errors({ stack: true }),
         format.colorize(),
         format.simple()
     )
@@ -11,7 +12,12 @@ export const consoleTransport = new transports.Console({
 
 export const logger = createLogger({
     level: process.env.LOG_LEVEL || 'http',
-    format: format.json(),
+    format: format.combine(
+        format.errors({ stack: false }),
+        format.uncolorize(),
+        format.timestamp(),
+        format.json()
+    ),
     transports: [
         consoleTransport,
         new transports.File({ filename: 'logs/error.log', level: 'error' }),
