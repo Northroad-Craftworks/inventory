9d5ac30b7d4e4a6c8ddfaab680e8cb9aca881431
James Tanner <james@northroad-craftworks.com>
2022-09-24 23:53:02 -0400

    Add file-based session store

--- src/middleware/session.js
+++ src/middleware/session.js
@@ -1,4 +1,9 @@
 import session from 'express-session';
+import FileStoreFactory from 'session-file-store';
+import logger from '../lib/logger.js';
+
+const FileStore = FileStoreFactory(session);
+// TODO Use a network-based filestore.
 
 const secret = process.env.SESSION_SECRET;
 if (!secret) throw new Error('Missing required SESSION_SECRET configuration');
@@ -9,5 +14,8 @@ export default session({
     name: 'NRCW-I',
     saveUninitialized: false,
     resave: false,
-    // TODO Configure a proper session store.
+    store: new FileStore({
+        // TODO Specify a path
+        logFn: (message) => logger.debug(message)
+    })
 })
